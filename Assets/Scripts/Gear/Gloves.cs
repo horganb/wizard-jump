a9611bf3326a46271df62fcdbfe60bc429d8c420
Ben Horgan <benhorgan68@gmail.com>
2024-07-02 11:44:36 -0400

    fix ui; remove gloves

--- Assets/Scripts/Gear/Gloves.cs
+++ Assets/Scripts/Gear/Gloves.cs
@@ -1,24 +1,22 @@
-﻿using GamGUI;
-
-namespace Gear
+﻿namespace Gear
 {
-    public class Gloves : Gear
-    {
-        public override string DisplayName()
-        {
-            return "Gloves of Quick Attack";
-        }
-
-        public override void Acquire()
-        {
-            PlayerGear.Instance.gloves.gameObject.SetActive(true);
-            Player.Instance.attackSpeed -= 0.1f;
-            GameGUI.Instance.DisplayMessage("Attack speed increased!");
-        }
-
-        protected override string SpriteName()
-        {
-            return "Gloves";
-        }
-    }
+    // public class Gloves : Gear
+    // {
+    //     public override string DisplayName()
+    //     {
+    //         return "Gloves of Quick Attack";
+    //     }
+    //
+    //     public override void Acquire()
+    //     {
+    //         PlayerGear.Instance.gloves.gameObject.SetActive(true);
+    //         Player.Instance.attackSpeed -= 0.1f;
+    //         GameGUI.Instance.DisplayMessage("Attack speed increased!");
+    //     }
+    //
+    //     protected override string SpriteName()
+    //     {
+    //         return "Gloves";
+    //     }
+    // }
 }

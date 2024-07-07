cc7d086106eaa7259fdc9d7a91fee78f03bb81bf
Ben Horgan <benhorgan68@gmail.com>
2024-07-07 12:28:23 -0400

    main menu

---
+++ Assets/Scripts/GamGUI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GamGUI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Level");
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}

96100de564eccfdc6e7298e6bb76242f9f23e799
Ben Horgan <benhorgan68@gmail.com>
2024-07-10 21:17:30 -0400

    sapphire reward

---
+++ Assets/Scripts/RewardType/GainSapphire.cs
@@ -0,0 +1,47 @@
+﻿using Interactable;
+using Save;
+using UnityEngine;
+
+namespace RewardType
+{
+    public class GainSapphire : RewardType, IChestReward
+    {
+        public Sprite sapphire;
+
+        public string Name()
+        {
+            return "15 Sapphire";
+        }
+
+        public void Acquire()
+        {
+            Player.Instance.sapphire += 15;
+            SaveManager.Save();
+        }
+
+        public Sprite GetSprite()
+        {
+            return sapphire;
+        }
+
+        public override IChestReward[] GenerateRewards()
+        {
+            return new IChestReward[] { this };
+        }
+
+        public override bool CanSpawn()
+        {
+            return true;
+        }
+
+        public override int GetCost()
+        {
+            return 30;
+        }
+
+        public override string PurchasePrompt()
+        {
+            return "Buy 15 sapphire";
+        }
+    }
+}
